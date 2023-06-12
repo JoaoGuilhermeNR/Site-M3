1090d5790fff795dfd28fa924ef8666659721ce7
João Guilherme Nobrega Rodrigues <66884159+JoaoGuilhermeNR@users.noreply.github.com>
2023-06-12 17:44:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* menu na rolagem */
 window.addEventListener('scroll', function() {
     var menu = document.getElementById('menu');
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -8,33 +9,38 @@ window.addEventListener('scroll', function() {
       menu.classList.remove('scrolled');
     }
   });
-  
-  const produtosSection = document.querySelector('#produtos');
-  const menuLinks = document.querySelectorAll('#menu a');
-  
-  menuLinks.forEach((link) => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault();
-      const href = link.getAttribute('href');
-      const offsetTop = produtosSection.offsetTop;
-  
-      window.scrollTo({
-        top: offsetTop,
-        behavior: 'smooth',
-      });
-    });
-  });
-  
-  window.addEventListener('scroll', () => {
-    const scrollPosition = window.pageYOffset;
-    const sectionOffset = produtosSection.offsetTop;
-    const windowHeight = window.innerHeight;
-  
-    if (scrollPosition > sectionOffset - windowHeight + 100) {
-      produtosSection.classList.add('scroll-active');
-    } else {
-      produtosSection.classList.remove('scroll-active');
-    }
-  });
 
-  
+  function smoothScroll(target) {
+    const element = document.querySelector(target);
+    window.scrollTo({
+        top: element.offsetTop,
+        behavior: 'smooth'
+    });
+}
+
+const menuLinks = document.querySelectorAll('.menu a');
+const menu = document.querySelector('.menu');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const target = this.getAttribute('href');
+        smoothScroll(target);
+        menu.classList.remove('open');
+    });
+});
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('open');
+});
+
+/* animação de rolagem */
+$(document).ready(function() {
+  $('.scroll-link').on('click', function(event) {
+      event.preventDefault();
+      var target = $(this).attr('href');
+      $('html, body').animate({
+          scrollTop: $(target).offset().top
+      }, 800);
+  });
+});

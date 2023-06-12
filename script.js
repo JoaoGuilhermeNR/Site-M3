306f3e6460af601e01502f9a6486197f1c585659
João Guilherme Nobrega Rodrigues <66884159+JoaoGuilhermeNR@users.noreply.github.com>
2023-06-12 16:35:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,5 +8,33 @@ window.addEventListener('scroll', function() {
       menu.classList.remove('scrolled');
     }
   });
+  
+  const produtosSection = document.querySelector('#produtos');
+  const menuLinks = document.querySelectorAll('#menu a');
+  
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+      const offsetTop = produtosSection.offsetTop;
+  
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth',
+      });
+    });
+  });
+  
+  window.addEventListener('scroll', () => {
+    const scrollPosition = window.pageYOffset;
+    const sectionOffset = produtosSection.offsetTop;
+    const windowHeight = window.innerHeight;
+  
+    if (scrollPosition > sectionOffset - windowHeight + 100) {
+      produtosSection.classList.add('scroll-active');
+    } else {
+      produtosSection.classList.remove('scroll-active');
+    }
+  });
 
   
